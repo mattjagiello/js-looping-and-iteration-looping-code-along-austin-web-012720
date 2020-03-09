184203a698989c314102eed5c4ec6cdaa65d5abd
Matthew Jagiello <mjagiello@gmail.com>
2020-03-09 05:21:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, eventname) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++){
+  messages.push(`Thank you, ${names[i]}, for the wonderful ${eventname} gift!`);
+  }
+  return messages;
+}
+
+function countDown(integer){
+  while (integer >= 0) {
+    console.log(integer)
+    integer --;
+  }
+}
