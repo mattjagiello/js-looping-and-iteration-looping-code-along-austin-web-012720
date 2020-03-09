af588266e97fa3b06fb977af1840b358582fd10e
Matthew Jagiello <mjagiello@gmail.com>
2020-03-09 05:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function writeCards(names, eventname) {
   messages = []
   for (let i = 0; i < names.length; i++){
-  messages.push()`Thank you ${names[i]} for the wonderful birthday gift!`);
+  messages.push(`Thank you ${names[i]} for the wonderful birthday gift!`);
   }
 }
